c02a9a8686fe27bbb46d2b2c8b76321fdaf8bdbb
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2024-06-18 20:16:39 -0300

    resolviendo problema de DB

--- api/src/db.js
+++ api/src/db.js
@@ -4,13 +4,10 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST , DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(
-  DB_DEPLOY ?DB_DEPLOY :`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'DB/database.sqlite'
+});
 console.log(DB_DEPLOY);
 // const sequelize = new Sequelize(
 //   DB_DEPLOY,
